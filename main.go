845012b946646206f5eb98fc8733967bd16a880a
Tae.Garam <drasla@naver.com>
2022-07-15 15:21:26 +0900

    #2.2 Methods part Two

--- main.go
+++ main.go
@@ -7,5 +7,11 @@ import (
 
 func main() {
 	account := accounts.NewAccount("nico")
+	account.Deposit(10)
+	fmt.Println(account)
+	err := account.Withdraw(20)
+	if err != nil {
+		fmt.Println(err)
+	}
 	fmt.Println(account)
 }
