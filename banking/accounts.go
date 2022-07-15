693f28bada6483bf9c509fb9787687f038d0d58b
Tae.Garam <drasla@naver.com>
2022-07-15 15:04:57 +0900

    #2.1 Methods part One

--- banking/accounts.go
+++ banking/accounts.go
@@ -11,3 +11,8 @@ func NewAccount(owner string) *Account {
 	account := Account{owner: owner, balance: 0}
 	return &account
 }
+
+// Deposit x amount on your account
+func (a Account) Deposit(amount int) {
+	a.balance += amount
+}
