8b0883507f4703536d308438efa5f0346540d631
Tae.Garam <drasla@naver.com>
2022-07-18 18:34:14 +0900

    #4.3 extractJob partTwo

--- main.go
+++ main.go
@@ -6,20 +6,65 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 )
 
 var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
 
-func main() {
-	totalPages := getPages()
-	for i := 0; i < totalPages; i++ {
-		getPage(i)
-	}
+type extractedJob struct {
+	id       string
+	title    string
+	location string
+	salary   string
+	summary  string
 }
 
-func getPage(page int) {
+func main() {
+	var jobs []extractedJob
+	totalPages := getPages()
+	for i := 0; i < totalPages; i++ {
+		extractedJobs := getPage(i)
+		jobs = append(jobs, extractedJobs...)
+	}
+
+	fmt.Println(jobs)
+}
+
+func getPage(page int) []extractedJob {
+	var jobs []extractedJob
 	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
 	fmt.Println("Requesting", pageURL)
+	res, err := http.Get(pageURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err2 := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err2)
+
+	searchCards := doc.Find(".jobsearch-SerpJobCard")
+
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		job := extractJob(card)
+		jobs = append(jobs, job)
+	})
+
+	return jobs
+}
+
+func extractJob(card *goquery.Selection) extractedJob {
+	id, _ := card.Attr("data-jk")
+	title := cleanString(card.Find(".title>a").Text())
+	location := cleanString(card.Find(".sjcl").Text())
+	salary := cleanString(card.Find(".salaryText").Text())
+	summary := cleanString(card.Find(".summary").Text())
+
+	return extractedJob{id: id, title: title, location: location, salary: salary, summary: summary}
+}
+
+func cleanString(str string) string {
+	return strings.Join(strings.Fields(strings.TrimSpace(str)), "")
 }
 
 func getPages() int {
@@ -30,8 +75,8 @@ func getPages() int {
 
 	defer res.Body.Close()
 
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	checkErr(err)
+	doc, err2 := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err2)
 
 	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
 		pages = s.Find("a").Length()
