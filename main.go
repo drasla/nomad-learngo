f77e6c8061aad3ca88f8502b47b42e1d4c07d5e2
Tae.Garam <drasla@naver.com>
2022-07-18 15:51:19 +0900

    #3.3 Channels

--- main.go
+++ main.go
@@ -6,13 +6,17 @@ import (
 )
 
 func main() {
-	go sexyCount("nico")
-	sexyCount("lynn")
+	c := make(chan bool)
+	people := [2]string{"nico", "lynn"}
+	for _, person := range people {
+		go isSexy(person, c)
+	}
+	fmt.Println(<-c)
+	fmt.Println(<-c)
 }
 
-func sexyCount(person string) {
-	for i := 0; i < 10; i++ {
-		fmt.Println(person, "is sexy", i)
-		time.Sleep(time.Second)
-	}
+func isSexy(person string, c chan bool) {
+	time.Sleep(time.Second * 5)
+	fmt.Println(person)
+	c <- true
 }
